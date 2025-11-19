6a1f871ba9692d7ca25b4f7fbd0f0331fb8e9f45
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-11-19 23:28:39 +0530

    almost done

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const title = document.getElementById("title");
 const amount = document.getElementById("amount");
 const btn = document.getElementById("btn");
+const list = document.getElementById("list");
 
 btn.addEventListener("click", () => {
-const expenseTitle = title.value.trim();
-const expenseAmount = amount.value.trim();
+  const expenseTitle = title.value.trim();
+  const expenseAmount = amount.value.trim();
 
-if(expenseTitle === "" || expenseAmount === "" ) {
-   alert("Kindly fill both");
-   return;
-}
+  if (expenseTitle === "" || expenseAmount === "") {
+    alert("Kindly fill both");
+    return;
+  }
 
+  const item = document.createElement("div"); //<div></div>
+  item.classList.add("item"); // <div class="item"></div>
+
+
+  item.innerHTML = `<p>${expenseTitle}</p>  <p>Rs.${expenseAmount}</p>
+  <button class="dltBtn">Delete</button>
+`;
+
+
+
+
+ const dltBtn = item.querySelector('.dltBtn');
+
+  dltBtn.addEventListener("click", () => {
+   item.remove();
+  })
+
+   list.appendChild(item);
+
+  title.value = "";
+  amount.value = "";
 
 });
-
