fc48739abca8e4c8d88a5a81f9b08212a033001f
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-11-21 13:10:47 +0530

    total done

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const amount = document.getElementById("amount");
 const btn = document.getElementById("btn");
 const list = document.getElementById("list");
 
+let expenses = [];
+
 btn.addEventListener("click", () => {
   const expenseTitle = title.value.trim();
   const expenseAmount = amount.value.trim();
@@ -12,28 +14,40 @@ btn.addEventListener("click", () => {
     return;
   }
 
-  const item = document.createElement("div"); //<div></div>
-  item.classList.add("item"); // <div class="item"></div>
+  const item = document.createElement("div");
+  item.classList.add("item");
 
+  item.innerHTML = `
+    <p>${expenseTitle}</p>
+    <p>₹${expenseAmount}</p>
+    <button class="dltBtn">Delete</button>
+  `;
 
-  item.innerHTML = `<p>${expenseTitle}</p>  <p>Rs.${expenseAmount}</p>
-  <button class="dltBtn">Delete</button>
-`;
+  // Add amount to array
+  expenses.push(Number(expenseAmount));
+  updateTotal();
 
-
-
-
- const dltBtn = item.querySelector('.dltBtn');
+  const dltBtn = item.querySelector('.dltBtn');
 
   dltBtn.addEventListener("click", () => {
-   item.remove();
-  })
+    // Remove that amount from array
+    expenses = expenses.filter((amt) => amt !== Number(expenseAmount));
+    updateTotal();
+    item.remove();
+  });
 
-   list.appendChild(item);
+  list.appendChild(item);
 
   title.value = "";
   amount.value = "";
-
 });
+
+function updateTotal() {
+  const totalAmount = expenses.reduce((sum, amt) => sum + amt, 0);
+  const totalItem = expenses.length
+  document.getElementById("totalAmount").innerText = `Total Amount: ₹${totalAmount}`;
+  document.getElementById("totalItem").innerText = `Total Items: ${totalItem}`
+
+}
 
 
